aef510318a739b3de291c9c82127ebba50d38c0c
Abrorbek Toshpo'latov <109383863+Abrorbek19@users.noreply.github.com>
2024-01-26 19:50:37 +0500

    show

--- resources/views/admin/menu/lessons/show.blade.php
+++ resources/views/admin/menu/lessons/show.blade.php
@@ -444,15 +444,7 @@ use App\Models\Test;
     </div>
 @endsection
 @section('summernote-editor-js')
-    {{--    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>--}}
-    {{--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>--}}
-{{--        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>--}}
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>--}}
-{{--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>--}}
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>--}}
-{{--    <script type="text/javascript" src="cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>--}}
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-
     <script type="text/javascript">
         $('#video_content').summernote({
             placeholder:'Tarif...',
@@ -475,7 +467,6 @@ use App\Models\Test;
             fontSizeUnits: ['px'],
         })
     </script>
-
     <script type="text/javascript">
         $(document).ready(function() {
             // Add new question button click event
@@ -552,6 +543,4 @@ use App\Models\Test;
             });
         });
     </script>
-
-
 @endsection
